8adec2a784f7622d3260c12dd818502628b05445
Mohnish <mohnish.a007@gmail.com>
2024-07-23 20:11:15 +0100

    basic navigation working fine

--- src/App.js
+++ src/App.js
@@ -6,16 +6,29 @@ import Posttest from './components/Posttest';
 import Deletetest from './components/Deletetest';
 import Puttest from './components/Puttest';
 import NotePage from './pages/NotePage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <>
     <BrowserRouter>
+    <ul>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+      <li>
+        <Link to='/signup'>Signup</Link>
+      </li>
+    </ul>
     <Routes>
       <Route index element={<NotePage/>} />
       <Route path='/login' element={<LoginPage/>} />
+      <Route path='/signup' element={<Signup/>} />
     </Routes>
     </BrowserRouter>
  
