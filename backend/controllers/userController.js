b24a3cf8fbf6fc71d5541e060775cc803b486a10
Mohnish <mohnish.a007@gmail.com>
2024-07-23 01:52:55 +0100

    basic signup with hashed password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,12 @@
 const user = require('../models/user');   // connected from the databases by model files
-
+const bcrypt = require('bcryptjs')
 const signup = async (req, res) => {
     // get email and password from req.body
     const { email, password } = req.body;
+    // hasing the password 
+    const hashedPassword = await bcrypt.hash(password, 10);  // 10 is the number of rounds for hashing
     // create user in the database
-    await user.create({ email,password});
+    await user.create({ email,password: hashedPassword });
 
     // respond after successful creation 
     res.sendStatus(200)
