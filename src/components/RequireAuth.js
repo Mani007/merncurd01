08c8c653152c8f8233b8f0c5f974cb8cea20b377
Mohnish <mohnish.a007@gmail.com>
2024-07-24 01:51:42 +0100

    Another way to navigate pages using react router

--- src/components/RequireAuth.js
+++ src/components/RequireAuth.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 // This is a special component for handling required authentication function. It is also higher order component or wrapper component.  
 import userStore from '../store/userStore'
 import LoginPage from '../pages/LoginPage';
+import { Navigate } from 'react-router-dom';
 function RequireAuth(props) {
     const store = userStore();
 
@@ -15,8 +16,9 @@ function RequireAuth(props) {
         return ( 
         <>
         <h1>You are not authenticated</h1> <br /> 
+        <p>Please login below</p> <br />
         {/* // Redirect to login page if not authenticated.  Replace {component} with your desired component.  This is just a placeholder.  Replace it with your actual component.  In your App.js, you will need to import and wrap your components in this component.   */}
-        <LoginPage/>
+        <Navigate to='/login'/> 
         </>
         )
       } else {
